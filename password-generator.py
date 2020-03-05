01c460114d4576eb07f46dee9d395ebf41519417
Greklln <jadenhoward9@gmail.com>
2020-03-04 19:09:49 -0600

    Inception
    created the program and successfully introduced functionality

---
+++ password-generator.py
@@ -0,0 +1,22 @@
+from random import choice
+
+password = ""
+words = open("words.txt", "r").readlines()
+words = [word.replace("\n", "_")for word in words]
+
+for i in range(0,4):
+    word = choice(words)
+    usable = True
+    while usable != True:
+        word = choice(words)
+        if len(word) < 8:
+            usable = False
+            words.remove(word)
+        else:
+            usable = True
+    password += word
+    words.remove(word)
+
+password = password[:-1]
+
+print(password)
