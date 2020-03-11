99acc5e325a9c3ca1b43d1429f30a0ae33429776
Greklln <jadenhoward9@gmail.com>
2020-03-11 17:39:34 -0500

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,21 +1,25 @@
 from random import choice
 
-password = ""
-words = open("words.txt", "r").readlines()
-words = [word.replace("\n", "_")for word in words]
+def createPassword():
+    password = ""
+    words = open("words.txt", "r").readlines()
+    words = [word.replace("\n", "_")for word in words]
 
-for i in range(0,4):
-    usable = False
-    while usable != True:
-        word = choice(words)
-        if len(word) < 8:
-            usable = False
-            words.remove(word)
-        else:
-            usable = True
-    password += word
-    words.remove(word)
+    for i in range(0,6):
+        usable = False
+        while usable != True:
+            word = choice(words)
+            if len(word) < 5:
+                usable = False
+                words.remove(word)
+            else:
+                usable = True
+        password += word
+        words.remove(word)
 
-password = password[:-1]
+    password = password[:-1]
 
-print(password)
+    print(password)
+
+
+createPassword()
