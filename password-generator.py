48f2f65f159c85919302f16bc23d699994a8c62a
Greklln <jadenhoward9@gmail.com>
2020-05-17 21:10:48 -0500

    Update password-generator.py
    removed tkinter, improved readability, and made the main function again

--- password-generator.py
+++ password-generator.py
@@ -1,47 +1,27 @@
-from tkinter import *
 from random import choice
 from os.path import isfile
 
-# Setup for the GUI
-root = Tk()
-root.title("Password Manager")
-root.geometry("1000x500")
-
-class Application(Frame):
-    # An interactive password manager
-    def __init__(self, master):
-        # Initialize the app's frame
-        super(Application, self).__init__(master)
-        self.grid()
-        self.create_widgets()
-
-    # Creates the buttons
-    def create_widgets(self):
-        self.getCredentialsBtn = Button(self, text = "Get Credentials")
-        self.getCredentialsBtn.grid()
-        self.createCredentialsBtn = Button(self, text = "Create Credentials")
-        self.createCredentialsBtn.grid()
-        self.websiteLabel = Label(self)
-        self.websiteLabel.grid()
-        self.passwordLabel = Label(self)
-
-
-app = Application(root)
-
 def createPassword():
+
     password = ""
     words = open("words.txt", "r").readlines()
-    words = [word.replace("\n", "_")for word in words]
+    words = [word.replace("\n", "_") for word in words]
 
     for i in range(0, 6):
+
         usable = False
+
         while not usable:
+
             word = choice(words)
             if len(word) < 5:
+
                 usable = False
                 words.remove(word)
+
             else:
                 usable = True
+
         password += word
         words.remove(word)
 
@@ -51,25 +31,59 @@ def createPassword():
 
 
 def createCredentials():
-    site = input("What site is this for? ").lower()
+
+    site = input("Site/Application Name: ").upper()
     sitePassword = createPassword()
-    if isfile(site + "File.txt"):
-        print(f"You already have credentials for {site}")
+
+    if isfile(site + "_File.txt"):
+
+        print(f"You already have credentials for {site}, GET them instead")
+
     else:
         siteFile = open(site + "File.txt", "w+")
         siteFile.write(f"{site} \n{sitePassword}")
         siteFile.close()
+        print(f"The password that was created is {sitePassword}")
 
 
 def getCredentials():
-    site = input("Which application do you need credentials for? ").lower()
-    if isfile(site + "File.txt"):
-        siteFile = open(site + "File.txt", "r")
-        siteFile = [item.replace("\n", "")for item in siteFile]
+
+    site = input("Site/Application Name: ").upper()
+
+    if isfile(site + "_File.txt"):
+
+        siteFile = open(site + "_File.txt", "r")
+        siteFile = [item.replace("\n", "") for item in siteFile]
         password = siteFile[1]
+        siteFile.close()
         print(f"Site/Application: {site} \nPassword: {password}")
+
     else:
-        print(f"You don't have any credentials stored for {site}")
+        print(f"You don't have any credentials stored for {site}, CREATE some instead")
+
+def useApp():
+
+    using = True
+
+    while using:
+
+        task = input("CREATE, GET, or CLOSE? ").upper()
+
+        if task == "CREATE":
+
+            createCredentials()
+
+        elif task == "GET":
+
+            getCredentials()
+
+        elif task == "CLOSE":
+
+            using = False
+
+        else:
+
+            print("That operation is not supported by this application, please use either GET or CREATE")
 
 
-root.mainloop()
+useApp()
